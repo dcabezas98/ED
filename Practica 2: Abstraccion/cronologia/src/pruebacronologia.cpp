4fb589529807be442c8f79b8bf35b1ed8f209097
David Cabezas <dxabezas@gmail.com>
2017-10-24 11:35:54 +0200

    Update pruebacronologia.cpp

--- Practica 2: Abstraccion/cronologia/src/pruebacronologia.cpp
+++ Practica 2: Abstraccion/cronologia/src/pruebacronologia.cpp
@@ -19,23 +19,72 @@ int main(int argc, char * argv[]){
    
    Cronologia mi_cronologia;
    f>>mi_cronologia; //Cargamos en memoria la cronología.
-
-   cout << mi_cronologia[0].year() << endl;
-   cout << mi_cronologia[0] << endl;
-   cout << mi_cronologia[1].year() << endl;
-   cout << mi_cronologia[1] << endl;
-   cout << mi_cronologia[2].year() << endl;
-   cout << mi_cronologia[2] << endl;
-
-   cout << mi_cronologia << endl;
+   
    /* Exhibir aquí la funcionalidad programada para el TDA Cronologia / TDA FechaHistorica */ 
 
    // Algunas sugerencias: 
    // - Obtener los eventos acaecidos en un año dado
+
+   int y, pos;
+   cout << "Introduzca el año que quiere consultar" << endl;
+   cin >> y;
+
+   pos = mi_cronologia.searchYear(y);
+
+   if (pos != -1){
+     cout << "Los acontecimientos ocurridos en el año " << y <<" son:\n";
+
+     for (int i=0; i< mi_cronologia[pos].nhechos(); i++){
+       cout << mi_cronologia[pos][i] << endl;
+     }
+	    
+   }else{
+     cout << "El año introducido no está en la cronología.\n";
+   }
+   
    // - Obtener la (sub)cronología de eventos históricos acaecidos en [anioDesde, anioHasta], donde anioDesde y anioHasta son proporcionados por el usuario
+   
+   int anioDesde, anioHasta;
+
+   cout << "Introduzca el año donde empieza la subcronología: ";
+   cin >> anioDesde;
+   cout << "Introduzca el año donde termina la subcronología: ";
+   cin >> anioHasta;
+
+   Cronologia subcronoA;
+   subcronoA= mi_cronologia.subcronologia(anioDesde, anioHasta);
+
+   cout << "La subcronologia que comienza en : " << anioDesde << " y termina en " << anioHasta << " es: \n" << subcronoA << endl;
+   
+
    // - Obtener la (sub)cronología de eventos históricos asociados a una palabra clave. Ejemplo: la cronología de eventos asociados a "Einstein"
+
+   string busca;
+o
+   cout << "Inserte cadena a buscar: ";
+   cin >> busca;
+
+   cout << "Subcronología que la contiene:\n" << mi_cronologia.subcronologia(busca) << endl;
+     
    // - Operadores sobre cronologías, ejemplo: Unión de dos cronologías (la cronología resultante debería estar ordenada)
-   // - Cualquier otra funcionalidad que consideréis de interés
 
+   Cronologia aUnir;
+   string nombre;
 
+   cout << "Ruta de la cronología a unir: ";
+   cin >> nombre;
+
+   ifstream f1 (nombre);
+   if (!f1){
+    cout<<"No puedo abrir el fichero "<< nombre <<endl;
+    return 0;
+   }
+
+   f1 >> aUnir;
+
+   cout << "Cronología resultante de unir la anterior cronología a la nueva:" << endl;
+
+   aUnir += mi_cronologia;
+
+   cout << aUnir << endl;
 }
