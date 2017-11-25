89e8101504cb5691c6084258ca83f6c8c5072c6e
David Cabezas <dxabezas@gmail.com>
2017-11-25 19:52:47 +0100

    reto 3, header

---
+++ Retos/reto3/Stack_List.h
@@ -0,0 +1,42 @@
+/*
+  Clase Pila implementada a partir de las funciones de la clase List de
+  la stl.
+
+  David Cabezas Berrido
+  Patricia Córdoba Hidalgo
+*/
+
+#ifndef __Stack_List__
+#define __Stack_List__
+
+template <class T>
+class Stack{
+
+  private:
+
+  list<T> datos;
+
+  public:
+
+  Stack();
+
+  Stack(const Stack&);
+
+  Stack& operator=(const Stack&);
+
+  bool empty() const;
+
+  T& top();
+
+  const T& top() const;
+
+  void push(const T&);
+
+  void pop();
+
+  int size() const;
+};
+
+#include "Stack_List.cpp"
+
+#endif
