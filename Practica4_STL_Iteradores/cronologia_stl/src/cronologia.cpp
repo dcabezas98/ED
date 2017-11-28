6ac236a0b4948f2f2b625dff4d1b976aaf7efc39
PATRICIA <patriciacorhid@gmail.com>
2017-11-28 14:01:03 +0100

    Implementar nuevas funciones e iteradores

--- Practica4_STL_Iteradores/cronologia_stl/src/cronologia.cpp
+++ Practica4_STL_Iteradores/cronologia_stl/src/cronologia.cpp
@@ -7,6 +7,22 @@
 
 using namespace std;
 
+iterator Cronologia::begin(){
+  return fechas.begin();
+}
+
+const_iterator Cronologia::begin() const{
+  return fechas.cbegin();
+}
+
+iterator Cronologia::end(){
+  return fechas.end();
+}
+
+const_iterator Cronologia::end() const{
+  return fechas.cend();
+}
+
 Cronologia::Cronologia():fechas(){}
 
 Cronologia::Cronologia(const Cronologia &original) :fechas(original.fechas){
@@ -42,15 +58,33 @@ Cronologia& Cronologia::operator+=(const Cronologia &crono){
 }
 
 Cronologia Cronologia::subcronologia(int a1, int a2) const{
+  Cronologia crono;
+
+  for(int i= a1; i<=a2; i++){
+    if(fechas.find(i) != fechas.end()){
+      crono.add(fechas[i]);
+    }
+  }
+  return crono;
 }
 
 Cronologia Cronologia::subcronologia(string s) const{
 
   Cronologia crono;
+  FechaHistorica fecha;
 
-  for(int i = 0; i < fechas.used(); i++)
-    if(fechas[i].contiene(s)) crono.add(fechas[i]);
-
+  for(auto itc=fechas.begin(); itc != fechas.end(); itc++){
+    for(auto itf=(*itc).begin(); itf != (*itc).end(); itf++){
+      if((*itf).find(s) != string::npos){
+        fecha.add(*itf);
+      }
+    }
+    if(!fecha.empty()){
+      fecha.year()=itc->second.year();
+      crono.add(fecha);
+      fecha.destruir();
+    }
+  }
   return crono;
 }
 
@@ -61,6 +95,16 @@ ostream& operator<<(ostream &flujo, const Cronologia &crono){
   }
 
   return flujo;
+}
+
+void ImprimeCronologia (const Cronologia &c, ostream &os){
+   Cronologia::const_iterator it;
+   for (it=c.begin(); it!=c.end();++it){
+       os<<(*it).first<<"#";          //año esta en el key del map
+       EventoHistorico::const_iterator it_ev;
+       for (it_ev=(*it).second.begin(); it_ev!=(*it).second.end();++it_ev)
+        os<<(*it_ev)<<"#";
+   }
 }
 
 istream& operator>>(istream &flujo, Cronologia &crono){
